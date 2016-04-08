b03b137b165dd0dad15838a0ad0b663bd3c81378
Rolands Strakis <roland.strakis@gmail.com>
2016-04-08 15:44:50 +0300

    Initial commit

---
+++ MySimpleProjectForTesting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MySimpleProjectForTesting.Startup))]
+namespace MySimpleProjectForTesting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
